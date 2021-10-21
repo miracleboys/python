cee3893315d9203818458b21be07467446f6d2e0
lrmy <1454813239@qq.com>
2021-10-21 11:55:11 +0800

    Day 5

--- study/pizza.py
+++ study/pizza.py
@@ -6,3 +6,6 @@ def make_pizza(size,*toppings):
     print("\nMaking a "+str(size)+"-inch pizza with the following toppings:")
     for topping in toppings:
         print("-"+topping)
+
+def show():
+    print("Hello World!")
